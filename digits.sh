7e4838c424d5ebe56801880985031fe9aa3f6b4a
shanleng <shan.leng@wisc.edu>
2023-10-17 14:02:19 -0500

    Finished digit calculation.

--- digits.sh
+++ digits.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+sum=0
+
+for ((i=1000;i<=2000;i++))
+do
+  if [[ "$i" =~ [01][01][01][01]$ ]]
+  then 
+    sum=$(($sum+$i))
+  fi
+done
+echo "The sum of numbers between [1000,2000] having digits only from {0,1} is $sum"

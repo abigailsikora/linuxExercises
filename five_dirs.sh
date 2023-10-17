421c910e07d46a788cec604a5cb950abd1c30e7e
yyyyyyyyuhan <147095674+yyyyyyyyuhan@users.noreply.github.com>
2023-10-17 15:02:26 -0500

    five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -7,8 +7,7 @@ for i in {1..5};do
 
     for j in {1..4};do
 	filename="$subdirectory/file$j"
-	for k in $(seq $j);do 
-	    printf "%d\n" "$j">> "$filename"
-	done
+ 	lines=$(seq $j)
+ 	printf "%s\n" $lines > "$filename"
     done
 done

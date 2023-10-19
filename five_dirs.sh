ac0cdba5c998f89d080dd11d2e6fb3cac1d29901
yyyyyyyyuhan <147095674+yyyyyyyyuhan@users.noreply.github.com>
2023-10-19 13:01:56 -0500

    five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 mkdir "five"
 
-for i in {1..5};do
-    subdirectory="five/dir$i"
-    mkdir "$subdirectory"
-
-    for j in {1..4};do
+for j in {1..4};do
 	filename="$subdirectory/file$j"
- 	lines=$(seq $j)
- 	printf "%s\n" $lines > "$filename"
+	lines=$(seq $j) 
+	printf "%s\n" $lines > "$filename"
+	for k in $(seq $j);do 
+	    printf "%d\n" "$j">> "$filename"
+	done
     done
 done

a35f2c43b92f18ba85c1972bcb41f8ee66394f43
weiangela <113031997+weiangela@users.noreply.github.com>
2023-10-19 14:28:01 -0500

    Update mean.sh

--- mean.sh
+++ mean.sh
@@ -1,9 +1,10 @@
 #!/bin/bash                                                                                                                                                   
 
-if [[ $# -ne 2 ]]; then # Recall: "-ne" checks integer inequality.                                                                                            
-echo "usage: $0 <column> [file.csv]" 1>&2 # write error message to stderr (below)                                                                             
-exit 0
+if [[ $# -eq 0 ]]; then
+    echo "usage: $0 <column> [file.csv]" 1>&2 # write error message to stderr (below)                                                                         
+    exit 0
+elif [[ $# -eq 1 ]]; then
+    cat /dev/stdin | cut -d , -f $1 | tail -n +2 | { sum=0; c=0; while read n; do sum=$(echo "scale=6; $sum + $n" | bc) ; c=$(($c + 1)); done; echo $(echo "scale=6; $sum / $c" | bc); }
+elif [[ $# -eq 2 ]]; then # Recall: "-ne" checks integer inequality.                                                                                          
+    cat $2 | cut -d , -f $1 | tail -n +2 | { sum=0; c=0; while read n; do sum=$(echo "scale=6; $sum + $n" | bc) ; c=$(($c + 1)); done; echo $(echo "scale=6; $sum / $c" | bc); }
 fi
-
-cat $2 | cut -d , -f $(($1 + 1)) | tail -n +2 | { sum=0; c=0; while read n; do sum=$(echo "scale=6; $sum + $n" | bc) ; c=$(($c + 1)); done; echo $(echo "scal\
-e=6; $sum / $c" | bc); }

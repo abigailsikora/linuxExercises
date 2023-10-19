15bab1f0ac40ff461ac09f61ed3911d10f71fe81
weiangela <113031997+weiangela@users.noreply.github.com>
2023-10-19 14:15:07 -0500

    Update mean.sh

--- mean.sh
+++ mean.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                                   
 
-if [[ $# -ne 2 ]]; then # Recall: "-ne" checks integer inequality.
-echo "usage: $0 <column> [file.csv]" 1>&2 # write error message to stderr (below)
+if [[ $# -ne 2 ]]; then # Recall: "-ne" checks integer inequality.                                                                                            
+echo "usage: $0 <column> [file.csv]" 1>&2 # write error message to stderr (below)                                                                             
 exit 0
 fi
 
+cat $2 | cut -d , -f $(($1 + 1)) | tail -n +2 | { sum=0; c=0; while read n; do sum=$(echo "scale=6; $sum + $n" | bc) ; c=$(($c + 1)); done; echo $(echo "scal\
+e=6; $sum / $c" | bc); }

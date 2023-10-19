b4397f8037466b93176cbad8cefcea81453c63cd
weiangela <113031997+weiangela@users.noreply.github.com>
2023-10-19 13:24:12 -0500

    Update five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,12 +1,21 @@
-#!/bin/bash
-mkdir "five"
+#!/bin/bash                                                                                                                                                     
+mkdir -p "five"
 
-for j in {1..4};do
-	filename="$subdirectory/file$j"
-	lines=$(seq $j) 
-	printf "%s\n" $lines > "$filename"
-	for k in $(seq $j);do 
-	    printf "%d\n" "$j">> "$filename"
-	done
+if [ ! -d "$five" ]
+then
+    echo "File creates successfully."
+else
+    echo "Cannot creat file"
+fi
+
+for i in {1..5};do
+    subdirectory="five/dir$i"
+    mkdir -p "$subdirectory"
+
+    for j in {1..4};do
+        filename="$subdirectory/file$j"
+        for k in $(seq $j);do
+            printf "%d\n" "$j">> "$filename"
+        done
     done
 done
